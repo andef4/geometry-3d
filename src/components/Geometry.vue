ecefee97e34dfafcbbf92e438efa490cf0a24d24
Fabio Anderegg <andef4@bfh.ch>
2017-10-06 14:01:31 +0200

    added ui for mirroring on a line

--- src/components/Geometry.vue
+++ src/components/Geometry.vue
@@ -17,19 +17,37 @@
       <action-button icon="rotate-right" caption="Clockwise" @click="rotateClockwiseOrigin" color="danger"></action-button>
       <action-button icon="rotate-left" caption="Counterclockwise" @click="rotateCounterclockwiseOrigin" color="danger"></action-button>
 
-    </div>
-
-    <div class="col-2">
-
       <span class="font-weight-bold">Stretch and contract</span>
       <action-button icon="arrow-left" second-icon="arrow-right" caption="Stretch" @click="moveDown" color="warning"></action-button>
       <action-button icon="arrow-right" second-icon="arrow-left" caption="Contract" @click="moveUp" color="warning"></action-button>
+
+    </div>
+
+    <div class="col-2">
 
       <span class="font-weight-bold">Sheer</span>
       <action-button icon="arrow-right" caption="Top to right" @click="moveDown" color="info"></action-button>
       <action-button icon="arrow-left" caption="Top to left" @click="moveUp" color="info"></action-button>
       <action-button icon="arrow-up" caption="Right to top" @click="moveDown" color="info"></action-button>
       <action-button icon="arrow-down" caption="Right to bottom" @click="moveUp" color="info"></action-button>
+
+      <span class="font-weight-bold">Mirror on line</span><br>
+      <div>y = a∙x + b</div>
+
+      <div class="form-group row">
+        <label for="a" class="col-sm-2 col-form-label">a:</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="a">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="b" class="col-sm-2 col-form-label">b:</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="b">
+        </div>
+      </div>
+
+      <action-button icon="arrow-right" caption="Mirror" @click="moveDown" color="primary"></action-button>
 
     </div>
   </div>
