f8f5ab75037ebceaf6088fe4e923db38cad7fe4b
Fabio Anderegg <andef4@bfh.ch>
2018-01-07 18:58:47 +0100

    Add tabs to UI

--- src/components/Geometry.vue
+++ src/components/Geometry.vue
@@ -11,112 +11,34 @@
         :d="d === '' ? 0 : d">
       </geometry-canvas>
     </div>
-    <div class="col-4">
+    <div class="col-3">
       <button class="btn btn-danger w-100 mb-2" @click="reset">Reset</button>
 
-      <b-form-group label="Implementations:">
-        <b-form-radio-group :checked="currentImplementation"
-                            @input="setImplementation"
-                            :options="implementations"
-                            stacked>
-        </b-form-radio-group>
-      </b-form-group>
-
-      <div class="row">
-        <div class="col-6">
-          <div>
-            <div class="mb-1 pt-1 font-weight-bold">Move</div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="x" @click="moveXup" color="danger" class="mr-2"></action-button>
-              <action-button icon="minus" caption="x" @click="moveXdown" color="danger"></action-button>
-            </div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="y" @click="moveYup" color="success" class="mr-2"></action-button>
-              <action-button icon="minus" caption="y" @click="moveYdown" color="success"></action-button>
-            </div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="z" @click="moveZup" class="mr-2"></action-button>
-              <action-button icon="minus" caption="z" @click="moveZdown"></action-button>
-            </div>
+      <b-tabs>
+        <b-tab title="Move" class="mt-2">
+          <div class="d-flex">
+            <action-button icon="plus" caption="x" @click="moveXup" color="danger" class="mr-2"></action-button>
+            <action-button icon="minus" caption="x" @click="moveXdown" color="danger"></action-button>
           </div>
-
-          <div>
-            <div class="mb-1 pt-1 font-weight-bold">Stretch</div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="x" @click="stretchX" color="danger" class="mr-2"></action-button>
-              <action-button icon="minus" caption="x" @click="contractX" color="danger"></action-button>
-            </div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="y" @click="stretchY" color="success" class="mr-2"></action-button>
-              <action-button icon="minus" caption="y" @click="contractY" color="success"></action-button>
-            </div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="z" @click="stretchZ" class="mr-2"></action-button>
-              <action-button icon="minus" caption="z" @click="contractZ"></action-button>
-            </div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="y" @click="moveYup" color="success" class="mr-2"></action-button>
+            <action-button icon="minus" caption="y" @click="moveYdown" color="success"></action-button>
           </div>
-
-          <div>
-            <div class="mb-1 pt-1 font-weight-bold">Shear</div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="Y ⮕ X" @click="shearYXup" color="danger" class="mr-2"></action-button>
-              <action-button icon="minus" caption="Y ⮕ X" @click="shearYXdown" color="danger"></action-button>
-            </div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="Z ⮕ X" @click="shearZXup" color="danger" class="mr-2"></action-button>
-              <action-button icon="minus" caption="Z ⮕ X" @click="shearZXdown" color="danger"></action-button>
-            </div>
-
-            <div class="d-flex">
-              <action-button icon="plus" caption="X ⮕ Y" @click="shearXYup" color="success" class="mr-2"></action-button>
-              <action-button icon="minus" caption="X ⮕ Y" @click="shearXYdown" color="success"></action-button>
-            </div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="Z ⮕ Y" @click="shearZYup" color="success" class="mr-2"></action-button>
-              <action-button icon="minus" caption="Z ⮕ Y" @click="shearZYdown" color="success"></action-button>
-            </div>
-
-            <div class="d-flex">
-              <action-button icon="plus" caption="X ⮕ Z" @click="shearXZup" class="mr-2"></action-button>
-              <action-button icon="minus" caption="X ⮕ Z" @click="shearXZdown"></action-button>
-            </div>
-            <div class="d-flex">
-              <action-button icon="plus" caption="Y ⮕ Z" @click="shearYZup" class="mr-2"></action-button>
-              <action-button icon="minus" caption="Y ⮕ Z" @click="shearYZdown"></action-button>
-            </div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="z" @click="moveZup" class="mr-2"></action-button>
+            <action-button icon="minus" caption="z" @click="moveZdown"></action-button>
           </div>
+        </b-tab>
 
-          <div>
-            <div class="mb-1 pt-1 font-weight-bold">Plane ax+by+cz+d=0</div>
-            <div class="form-group row">
-              <label for="a" class="col-sm-2 col-form-label">a:</label>
-              <div class="col-sm-10">
-                <input type="number" step="0.1" class="form-control form-control-sm" id="a" v-model.number="a">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="b" class="col-sm-2 col-form-label">b:</label>
-              <div class="col-sm-10">
-                <input type="number" step="0.1" class="form-control form-control-sm" id="b" v-model.number="b">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="c" class="col-sm-2 col-form-label">c:</label>
-              <div class="col-sm-10">
-                <input type="number" step="0.1" class="form-control form-control-sm" id="c" v-model.number="c">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label for="d" class="col-sm-2 col-form-label">d:</label>
-              <div class="col-sm-10">
-                <input type="number" step="1" class="form-control form-control-sm" id="d" v-model.number="d">
-              </div>
-            </div>
-            <action-button icon="arrow-right" caption="Mirror" color="primary" @click="mirror"></action-button>
-            <action-button icon="arrow-right" caption="Project" color="success" @click="projectOnPlane"></action-button>
-          </div>
-        </div>
-        <div class="col-6">
+        <b-tab title="Rotate">
+          <b-form-group label="Implementations:">
+            <b-form-radio-group :checked="currentImplementation"
+                                @input="setImplementation"
+                                :options="implementations"
+                                stacked>
+            </b-form-radio-group>
+          </b-form-group>
+
           <div>
             <div class="mb-1 pt-1 font-weight-bold">Rotate around axis</div>
             <div class="d-flex">
@@ -148,9 +70,83 @@
               <action-button icon="rotate-left" caption="z" @click="rotateCenterCounterClockwiseZ"></action-button>
             </div>
           </div>
+        </b-tab>
 
-          <div class="mt-1">
-            <div class="mb-1 pt-1 font-weight-bold">Perspective projection</div>
+        <b-tab title="Stretch & Shear">
+          <div class="mb-1 pt-1 font-weight-bold">Stretch</div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="x" @click="stretchX" color="danger" class="mr-2"></action-button>
+            <action-button icon="minus" caption="x" @click="contractX" color="danger"></action-button>
+          </div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="y" @click="stretchY" color="success" class="mr-2"></action-button>
+            <action-button icon="minus" caption="y" @click="contractY" color="success"></action-button>
+          </div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="z" @click="stretchZ" class="mr-2"></action-button>
+            <action-button icon="minus" caption="z" @click="contractZ"></action-button>
+          </div>
+
+          <div class="mb-1 pt-1 font-weight-bold">Shear</div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="Y ⮕ X" @click="shearYXup" color="danger" class="mr-2"></action-button>
+            <action-button icon="minus" caption="Y ⮕ X" @click="shearYXdown" color="danger"></action-button>
+          </div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="Z ⮕ X" @click="shearZXup" color="danger" class="mr-2"></action-button>
+            <action-button icon="minus" caption="Z ⮕ X" @click="shearZXdown" color="danger"></action-button>
+          </div>
+
+          <div class="d-flex">
+            <action-button icon="plus" caption="X ⮕ Y" @click="shearXYup" color="success" class="mr-2"></action-button>
+            <action-button icon="minus" caption="X ⮕ Y" @click="shearXYdown" color="success"></action-button>
+          </div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="Z ⮕ Y" @click="shearZYup" color="success" class="mr-2"></action-button>
+            <action-button icon="minus" caption="Z ⮕ Y" @click="shearZYdown" color="success"></action-button>
+          </div>
+
+          <div class="d-flex">
+            <action-button icon="plus" caption="X ⮕ Z" @click="shearXZup" class="mr-2"></action-button>
+            <action-button icon="minus" caption="X ⮕ Z" @click="shearXZdown"></action-button>
+          </div>
+          <div class="d-flex">
+            <action-button icon="plus" caption="Y ⮕ Z" @click="shearYZup" class="mr-2"></action-button>
+            <action-button icon="minus" caption="Y ⮕ Z" @click="shearYZdown"></action-button>
+          </div>
+        </b-tab>
+
+        <b-tab title="Plane">
+            <div class="mb-1 pt-1 font-weight-bold">ax+by+cz+d=0</div>
+            <div class="form-group row">
+              <label for="a" class="col-sm-2 col-form-label">a:</label>
+              <div class="col-sm-10">
+                <input type="number" step="0.1" class="form-control form-control-sm" id="a" v-model.number="a">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="b" class="col-sm-2 col-form-label">b:</label>
+              <div class="col-sm-10">
+                <input type="number" step="0.1" class="form-control form-control-sm" id="b" v-model.number="b">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="c" class="col-sm-2 col-form-label">c:</label>
+              <div class="col-sm-10">
+                <input type="number" step="0.1" class="form-control form-control-sm" id="c" v-model.number="c">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="d" class="col-sm-2 col-form-label">d:</label>
+              <div class="col-sm-10">
+                <input type="number" step="1" class="form-control form-control-sm" id="d" v-model.number="d">
+              </div>
+            </div>
+            <action-button icon="arrow-right" caption="Mirror" color="primary" @click="mirror"></action-button>
+            <action-button icon="arrow-right" caption="Project" color="success" @click="projectOnPlane"></action-button>
+        </b-tab>
+
+        <b-tab title="Perspective projection" class="mt-2">
             <label for="xIntercept">x intercept:</label>
             <input type="number" step="1" class="form-control form-control-sm" id="xIntercept" v-model.number="xIntercept">
             <label for="yIntercept">y intercept:</label>
@@ -158,9 +154,8 @@
             <label for="yIntercept">z intercept:</label>
             <input type="number" step="1" class="form-control form-control-sm" id="zIntercept" v-model.number="zIntercept">
             <action-button class="mt-3" icon="video-camera" caption="Project" color="danger" @click="perspectiveProjection"></action-button>
-          </div>
-        </div>
-      </div>
+        </b-tab>
+      </b-tabs>
     </div>
   </div>
 </template>
